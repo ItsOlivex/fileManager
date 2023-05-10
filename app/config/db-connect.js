f9656e61890abab4f53516477da1119e1487ab4d
Mirko Olivetti <mirko.olivetti.f@gmail.com>
2023-05-10 15:48:42 +0200

    d

--- app/config/db-connect.js
+++ app/config/db-connect.js
@@ -19,7 +19,7 @@ class dbConnection {
 	constructor(next) {
 		this.db = mysql.createConnection({
 			host: "myfilesddns.ddns.net",
-			user: "mac",
+			user: "pc",
 			password: "Motocross2004!6cpbhh42ay5te",
 			database: "myfiles"
 		});
